8088e3207087e27a2a26c0a47130ef44f8086565
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-01-04 15:57:35 -0500

    * Adds functionality to test egress rules
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- libraries/_aws.rb
+++ libraries/_aws.rb
@@ -2,6 +2,6 @@
 # loaded only if AWS resources are needed.
 
 require 'aws-sdk' # TODO: split once ADK v3 is in use
-require '_aws_backend_factory_mixin'
-require '_aws_resource_mixin'
-require '_aws_connection'
+require_relative '/Users/dromazos/Documents/MITRE/cms-demo/inspec-aws/libraries/_aws_backend_factory_mixin'
+require_relative '/Users/dromazos/Documents/MITRE/cms-demo/inspec-aws/libraries/_aws_resource_mixin'
+require_relative '/Users/dromazos/Documents/MITRE/cms-demo/inspec-aws/libraries/_aws_connection'
